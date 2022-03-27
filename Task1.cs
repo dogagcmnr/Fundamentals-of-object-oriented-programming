d5e4818c4774b7556c02a9e2a6a86c5724359e8b
Doğa Göçmener <dogagocmener@student.cag.edu.tr>
2022-03-27 20:42:55 +0200

    Create Task1.cs

---
+++ Task1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudenrNR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("47388");
+            Console.ReadKey();
+        }
+    }
+}
